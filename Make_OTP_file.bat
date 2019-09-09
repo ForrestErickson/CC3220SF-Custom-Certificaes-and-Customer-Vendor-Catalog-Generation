f5c67fdbee004d08d59c34d38590e4959f9a9608
Lee Erickson <lee@spectrumtechniques.com>
2019-09-09 16:53:12 -0400

    Get SLImageCreator to make some file I think is a certificate catalog from batch file.

--- Make_OTP_file.bat
+++ Make_OTP_file.bat
@@ -6,14 +6,17 @@ REM Author: Forrest Lee Erickson
 REM Date: 20190909 
 REM Warranty: This program is designed to kill you but is not guarenteed to do so. Use at own risk.
 REM file naming as per or similar to: https://e2e.ti.com/support/wireless-connectivity/wifi/f/968/t/766413
-REM Run this file after creating keys and certificates by running the file: Make_key_csr.bat 
+REM Run this file after creating keys and certificates by first running the file: Make_key_csr.bat 
 REM #-------------------------------------------------------------
 
-clear 
+CLEAR
 ECHO Making OTP file for CC3220
 ECHO ...
 
 SETLOCAL
+REM set MYPATH=C:\Users\Public\Documents\keys_20190909
+set MYPATH=%~dp0
+ECHO This is my path %MYPATH%
 
 set SLIMAGECREATORPATH=C:\ti\uniflash_5.1.0\simplelink\imagecreator\bin\
 set "CERT_FOLDER=Certs"
@@ -22,16 +25,19 @@ REM echo CERT_FOLDER
 ECHO ...
 ECHO Simple Link Image Creator Version.
 %SLIMAGECREATORPATH%SLImageCreator.exe --version
+dir "%MYPATH%" 
 
 ECHO ...
 ECHO Creating CA Catalog.
-%SLIMAGECREATORPATH%SLImageCreator.exe tools make_cert_catalog --cert_folder "."  --out_file "foo2.txt"
+REM %SLIMAGECREATORPATH%SLImageCreator.exe tools make_cert_catalog --cert_folder "."  --out_file "foo2.txt"
+REM %SLIMAGECREATORPATH%SLImageCreator.exe tools make_cert_catalog --cert_folder "." --out_file "%MYPATH%Certs\foo2.lst"
+REM %SLIMAGECREATORPATH%SLImageCreator.exe tools make_cert_catalog --cert_folder "%MYPATH%" --out_file "%MYPATH%\Certs\foo2.lst"
+REM %SLIMAGECREATORPATH%SLImageCreator.exe tools make_cert_catalog --cert_folder %MYPATH%Certs\.. --out_file "%MYPATH%Certs\foo2.lst"
+REM %SLIMAGECREATORPATH%SLImageCreator.exe tools make_cert_catalog --cert_folder %MYPATH%%CERT_FOLDER% --out_file "%MYPATH%%CERT_FOLDER%\foo3.lst"
 
+%SLIMAGECREATORPATH%SLImageCreator.exe tools make_cert_catalog --cert_folder %MYPATH%%CERT_FOLDER% --out_file "%MYPATH%%CERT_FOLDER%\certificate_Catalog.lst"
 
 ECHO ...
 ECHO You made it to the end of the OTP file creation phase.
-rem dir .\Certs
 pause
-
-
 ENDLOCAL
